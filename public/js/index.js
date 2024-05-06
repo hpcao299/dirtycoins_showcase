e9e562b65073ac103c61ffeb00a53bbed88b5eaa
hpcao299 <caohoangphuc2909@gmail.com>
2024-05-06 20:33:05 +0700

    full page scroll functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new fullpage('#fullpage', {
+        controlArrows: false,
+        scrollbar: true,
+        onLeave(origin, destination, direction, trigger) {
+            const currentSection = origin.item;
+            const nextSection = destination.item;
 
+            const animatedOutElements = currentSection.querySelectorAll('.item-group > *');
+            const animatedInElements = nextSection.querySelectorAll(`.item-group > *`);
+
+            animatedOutElements.forEach(element => {
+                element.classList.add('xyz-out');
+                element.classList.remove('xyz-in');
+            });
+
+            animatedInElements.forEach(element => {
+                element.classList.remove('xyz-out');
+                element.classList.add('xyz-in');
+            });
+        },
+    });
+
+    const activeSection = document.querySelector('.section.active');
+    const animatedInElements = activeSection.querySelectorAll(`.item-group > *`);
+
+    animatedInElements.forEach(element => {
+        element.classList.remove('xyz-out');
+        element.classList.add('xyz-in');
+    });
+});
